6da3289f55ef2c71f066586568be2d9564daf2ba
chuuuul <chuuuul@naver.com>
2020-08-11 11:27:44 +0900

    리팩토링 : const value 정리

--- app/src/main/java/com/chul/weather/util/Const.kt
+++ app/src/main/java/com/chul/weather/util/Const.kt
@@ -1,5 +1,5 @@
 package com.chul.weather.util
 
+const val LOC_CODE_SEOUL: String = "1132599"
 const val LOC_CODE_LONDON: String = "44418"
 const val LOC_CODE_CHICAGO: String = "2379574"
-const val LOC_CODE_SEOUL: String = "1132599"
