a3f98859149e32cd0311231a3444d4f2f5016671
chuuuul <chuuuul@naver.com>
2020-08-11 11:27:10 +0900

    기능 : RecyclerView의 Ext 함수 생성  - item을 넣어주는 데이터 바인딩 함수 생성

---
+++ app/src/main/java/com/chul/weather/ext/RecyclerViewExt.kt
@@ -0,0 +1,21 @@
+package com.chul.weather.ext
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.chul.weather.data.model.WeatherAdapterModel
+import com.chul.weather.ui.WeatherAdapter
+
+@BindingAdapter("android:items")
+fun RecyclerView.setAdapter(items: MutableList<WeatherAdapterModel>?) {
+    if (items == null) {
+        return
+    }
+
+    when (this.adapter) {
+        is WeatherAdapter -> {
+            val adapter = this.adapter as WeatherAdapter
+            adapter.addNewItem(items)
+        }
+        else -> return
+    }
+}
