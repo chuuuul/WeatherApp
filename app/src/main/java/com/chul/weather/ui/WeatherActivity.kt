617d6fb1a6bf4214f114223912021bf674726419
chuuuul <chuuuul@naver.com>
2020-08-11 13:14:27 +0900

    기능 : ViewModel 기능 추가  - 3가지 위치에 대해 6일간 날씨 데이터를 가져오는 함수 작성  - 가져오기가 모두 진행 되었다면 업데이트 하는 함수 작성

--- app/src/main/java/com/chul/weather/ui/WeatherActivity.kt
+++ app/src/main/java/com/chul/weather/ui/WeatherActivity.kt
@@ -2,7 +2,10 @@ package com.chul.weather.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.chul.weather.R
+import com.chul.weather.databinding.ActivityWeatherBinding
+import kotlinx.android.synthetic.main.activity_weather.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class WeatherActivity : AppCompatActivity() {
@@ -11,8 +14,20 @@ class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weather)
 
-        vm.getWeather()
+        val binding = DataBindingUtil.setContentView<ActivityWeatherBinding>(this, R.layout.activity_weather)
+
+        binding.lifecycleOwner = this
+        binding.vm = vm
+
+        settingInit()
+    }
+
+    private fun settingInit() {
+        settingAdapter()
+    }
+
+    private fun settingAdapter() {
+        rv_weather_list.adapter = WeatherAdapter()
     }
 }
--- app/src/main/java/com/chul/weather/ui/WeatherViewModel.kt
+++ app/src/main/java/com/chul/weather/ui/WeatherViewModel.kt
@@ -1,32 +1,101 @@
 package com.chul.weather.ui
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.chul.weather.data.model.WeatherInfo
+import com.chul.weather.data.model.CategoryInfo
+import com.chul.weather.data.model.WeatherAdapterModel
 import com.chul.weather.data.repository.WeatherRepositoryImpl
+import com.chul.weather.util.LOC_CODE_LONDON
 import com.chul.weather.util.LOC_CODE_SEOUL
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class WeatherViewModel(private val repository: WeatherRepositoryImpl) : ViewModel() {
 
-    private val seoulWeatherInfo = mutableListOf<WeatherInfo>()
-
-
     private val compositeDisposable = CompositeDisposable()
-    fun getWeather() {
-        repository.getWeather(LOC_CODE_SEOUL, 6)
 
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("_chul", "데이터 넘어왔다 : $it")
-            }, {
-                it.printStackTrace()
-            }, {
-                Log.d("_chul", "이건 컴플리트")
-            })
-            .addTo(compositeDisposable)
+    private val seoulWeatherList = mutableListOf<WeatherAdapterModel>()
+    private val londonWeatherList = mutableListOf<WeatherAdapterModel>()
+    private val chicagoWeatherList = mutableListOf<WeatherAdapterModel>()
+
+    private val _weatherAdapterModelList = MutableLiveData<MutableList<WeatherAdapterModel>>(mutableListOf())
+    val weatherAdapterModelList: LiveData<MutableList<WeatherAdapterModel>> = _weatherAdapterModelList
+
+    private val updateSubject = PublishSubject.create<Unit>()
+
+
+    init {
+        adapterUpdateObserve()
+        getWeather()
     }
 
+    private fun getWeather() {
+        repository.getWeather(LOC_CODE_SEOUL, 6)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    seoulWeatherList.add(WeatherAdapterModel(CategoryInfo("Seoul")))
+                }
+                .subscribe({
+                    seoulWeatherList.add(WeatherAdapterModel(it))
+                }, {
+                    Log.e("_chul", "${it.message}")
+                }, {
+                    Log.d("_chul", "On Complete")
+                    seoulWeatherList.sortWith(compareBy({ it.type }, { it.weatherInfo?.date }))
+                    updateSubject.onNext(Unit)
+                })
+                .addTo(compositeDisposable)
+
+        repository.getWeather(LOC_CODE_LONDON, 6)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    londonWeatherList.add(WeatherAdapterModel(CategoryInfo("London")))
+                }.delay(1000L, TimeUnit.MILLISECONDS, Schedulers.computation())
+                .subscribe({
+                    londonWeatherList.add(WeatherAdapterModel(it))
+                }, {
+                    Log.e("_chul", "${it.message}")
+                }, {
+                    Log.d("_chul", "On Complete")
+                    londonWeatherList.sortWith(compareBy({ it.type }, { it.weatherInfo?.date }))
+                    updateSubject.onNext(Unit)
+                })
+                .addTo(compositeDisposable)
+
+        repository.getWeather(LOC_CODE_LONDON, 6)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    chicagoWeatherList.add(WeatherAdapterModel(CategoryInfo("Chicago")))
+                }.delay(1000L, TimeUnit.MILLISECONDS, Schedulers.computation())
+                .subscribe({
+                    chicagoWeatherList.add(WeatherAdapterModel(it))
+                }, {
+                    Log.e("_chul", "${it.message}")
+                }, {
+                    Log.d("_chul", "On Complete")
+                    chicagoWeatherList.sortWith(compareBy({ it.type }, { it.weatherInfo?.date }))
+                    updateSubject.onNext(Unit)
+                })
+                .addTo(compositeDisposable)
+
+
+    }
+
+    private fun adapterUpdateObserve() {
+        updateSubject.buffer(3, 3)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    val mutableList = mutableListOf<WeatherAdapterModel>()
+                    mutableList.addAll(seoulWeatherList)
+                    mutableList.addAll(londonWeatherList)
+                    mutableList.addAll(chicagoWeatherList)
+                    _weatherAdapterModelList.value = mutableList
+                }.addTo(compositeDisposable)
+    }
 }
