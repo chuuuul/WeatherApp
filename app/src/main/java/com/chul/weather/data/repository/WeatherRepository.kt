1491bec08ec208ffdc8d446a9df5f32f82be09ed
chuuuul <chuuuul@naver.com>
2020-08-12 19:52:41 +0900

    기능 : WeatherRepository 작성  - 캐시 저장하기 구현  - 캐시 가져오기 구현

--- app/src/main/java/com/chul/weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/chul/weather/data/repository/WeatherRepository.kt
@@ -1,14 +1,14 @@
 package com.chul.weather.data.repository
 
 import com.chul.weather.data.model.WeatherInfo
+import io.reactivex.Flowable
 import io.reactivex.Single
 import java.time.LocalDate
 
 interface WeatherRepository {
     fun getWeather(locationCode: String, date: LocalDate? = null): Single<WeatherInfo>
-    fun getWeather(
-        locationCode: String,
-        duration: Int,
-        startDate: LocalDate? = null
-    ): Single<List<WeatherInfo>>
+
+    fun getWeather(locationCode: String, duration: Int, startDate: LocalDate? = null)
+            : Flowable<List<WeatherInfo>>
+
 }
