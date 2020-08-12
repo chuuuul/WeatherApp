a25219f5ad52c44c838d6ca9388ceba141c51a41
chuuuul <chuuuul@naver.com>
2020-08-12 19:41:00 +0900

    버그 : TypeConverter에서 LocalDate 파싱 못하는 버그 수정  - LocalDate를 이해 할 수 있도록 타입 추가

--- app/src/main/java/com/chul/weather/data/source/local/room/Converters.kt
+++ app/src/main/java/com/chul/weather/data/source/local/room/Converters.kt
@@ -2,20 +2,50 @@ package com.chul.weather.data.source.local.room
 
 import androidx.room.TypeConverter
 import com.chul.weather.data.model.WeatherInfo
-import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.TypeAdapter
 import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonToken
+import java.io.IOException
+import java.time.LocalDate
 
 class Converters {
 
+    internal class LocalDateAdapter : TypeAdapter<LocalDate?>() {
+
+        @Throws(IOException::class)
+        override fun read(jsonReader: JsonReader): LocalDate? {
+            return if (jsonReader.peek() === JsonToken.NULL) {
+                jsonReader.nextNull()
+                null
+            } else {
+                LocalDate.parse(jsonReader.nextString())
+            }
+        }
+
+        override fun write(jsonWriter: com.google.gson.stream.JsonWriter?, localDate: LocalDate?) {
+            if (localDate == null) {
+                jsonWriter?.nullValue()
+            } else {
+                jsonWriter?.value(localDate.toString())
+            }
+        }
+    }
+
+    private val gson =
+        GsonBuilder().registerTypeAdapter(LocalDate::class.java, LocalDateAdapter().nullSafe())
+            .create()
+
     @TypeConverter
     fun weatherInfoList2json(weatherInfoList: List<WeatherInfo>): String {
-        val gson = Gson()
         return gson.toJson(weatherInfoList)
     }
 
     @TypeConverter
     fun json2WeatherInfoList(json: String): List<WeatherInfo> {
         val listType = object : TypeToken<List<WeatherInfo>>() {}.type
-        return Gson().fromJson(json, listType)
+        return gson.fromJson(json, listType)
     }
 }
+
