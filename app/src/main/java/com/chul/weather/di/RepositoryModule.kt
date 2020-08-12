1d99cea19bd5c7858f6b55f2d4a884187d73bdfc
chuuuul <chuuuul@naver.com>
2020-08-12 19:53:40 +0900

    기능 : RepositoryModule 수정

--- app/src/main/java/com/chul/weather/di/RepositoryModule.kt
+++ app/src/main/java/com/chul/weather/di/RepositoryModule.kt
@@ -1,12 +1,32 @@
 package com.chul.weather.di
 
+import androidx.room.Room
 import com.chul.weather.data.repository.WeatherRepositoryImpl
+import com.chul.weather.data.source.local.WeatherLocalDataSource
+import com.chul.weather.data.source.local.WeatherLocalDataSourceImpl
+import com.chul.weather.data.source.local.room.WeatherDao
+import com.chul.weather.data.source.local.room.WeatherDatabase
 import com.chul.weather.data.source.remote.WeatherRemoteDataSource
 import com.chul.weather.data.source.remote.WeatherRemoteDataSourceImpl
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val repositoryModule = module {
+    single<WeatherDatabase> {
+        Room.databaseBuilder(
+            androidContext(),
+            WeatherDatabase::class.java,
+            "weather.db"
+        ).build()
+    }
+    single<WeatherDao> { get<WeatherDatabase>().getWeatherDao() }
+
+    single<WeatherLocalDataSource> { WeatherLocalDataSourceImpl(get()) }
     single<WeatherRemoteDataSource> { WeatherRemoteDataSourceImpl(get()) }
-    single<WeatherRepositoryImpl> { WeatherRepositoryImpl(get()) }
+
+    single<WeatherRepositoryImpl> {
+        WeatherRepositoryImpl(get(), get())
+    }
+
 
 }
