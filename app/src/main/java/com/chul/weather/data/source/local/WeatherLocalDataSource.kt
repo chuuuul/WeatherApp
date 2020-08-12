8f672cb318eed7dd7b79de9123b54d2ac50fcac3
chuuuul <chuuuul@naver.com>
2020-08-12 14:08:46 +0900

    기능 : Local Data Source 작성

---
+++ app/src/main/java/com/chul/weather/data/source/local/WeatherLocalDataSource.kt
@@ -0,0 +1,12 @@
+package com.chul.weather.data.source.local
+
+import com.chul.weather.data.model.WeatherEntity
+import com.chul.weather.data.model.WeatherInfo
+import io.reactivex.Completable
+import io.reactivex.Single
+
+interface WeatherLocalDataSource {
+    fun saveWeather(locationCode: String, weatherList: List<WeatherInfo>)
+    fun loadWeather(locationCode: String): Single<WeatherEntity>
+    fun deleteWeather(locationCode: String): Completable
+}
