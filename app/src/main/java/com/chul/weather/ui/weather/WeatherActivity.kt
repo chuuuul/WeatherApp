c88669d6156bb78459546821698ea25e8388c582
chuuuul <chuuuul@naver.com>
2020-08-13 16:23:57 +0900

    기능 : 네트워크 연결 시에만 앱 동작하도록 변경

--- app/src/main/java/com/chul/weather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/chul/weather/ui/weather/WeatherActivity.kt
@@ -1,14 +1,20 @@
 package com.chul.weather.ui.weather
 
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.chul.weather.R
 import com.chul.weather.databinding.ActivityWeatherBinding
 import com.chul.weather.ui.weather.adapter.WeatherAdapter
 import com.chul.weather.ui.weather.adapter.WeatherDividerDecoration
+import com.chul.weather.util.NetworkCheckHelper
 import kotlinx.android.synthetic.main.activity_weather.*
+import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class WeatherActivity : AppCompatActivity() {
 
@@ -18,7 +24,8 @@ class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView<ActivityWeatherBinding>(this, R.layout.activity_weather)
+        val binding =
+            DataBindingUtil.setContentView<ActivityWeatherBinding>(this, R.layout.activity_weather)
 
         binding.lifecycleOwner = this
         binding.vm = vm
@@ -28,7 +35,18 @@ class WeatherActivity : AppCompatActivity() {
 
     private fun settingInit() {
         settingAdapter()
+        settingObserve()
+        checkNetwork()
     }
+
+    private fun settingObserve() {
+        vm.getWeatherEvent.observe(this, Observer {
+            it?.getContentIfNotHandled()?.apply {
+                vm.getAllWeather()
+            }
+        })
+    }
+
 
     private fun settingAdapter() {
         rv_weather_list.adapter =
@@ -38,5 +56,21 @@ class WeatherActivity : AppCompatActivity() {
                 this
             )
         )
+    }
+
+    private fun checkNetwork() {
+        if (!networkCheckHelper.isInternetAvailable()) {
+            AlertDialog.Builder(this)
+                .setMessage("인터넷이 연결되어 있지 않습니다.")
+                .setCancelable(false)
+                .setPositiveButton(
+                    "확인"
+                ) { _, _ ->
+                    Log.d("_chul", "네트워크 미 연결 - 종료")
+                    finish()
+                }.show()
+        } else {
+            vm.networkStateOn()
+        }
     }
 }
